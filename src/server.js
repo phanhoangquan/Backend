a30e1305131d55b8cccf45b308eb4d9df536ba0b
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-12-11 18:50:41 +0700

    Connection MySQL with NodeJS

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express()
 const configViewEngine = require('./config/viewEngine')
 const port = process.env.PORT || 3000
 const webRoutes = require('./routes/web')
+const mysql = require('mysql2');
 
 //config template engine
 configViewEngine(app)
@@ -12,6 +13,23 @@ configViewEngine(app)
 //Routes
 app.use('/',webRoutes)
 
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+});
+
+// A simple SELECT query
+connection.query(
+  'SELECT * FROM `User` u',
+  function(err, results, fields) {
+    console.log("Results: ", results); // results contains rows returned by server
+    console.log("Fields: ", fields); // fields contains extra meta data about results, if available
+  }
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
